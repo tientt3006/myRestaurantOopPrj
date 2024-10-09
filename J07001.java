54e275774ecd20e2476c4de8db33c296b831f9c9
huongcnc <huonghanoi181@gmail.com>
2024-10-09 21:17:13 +0700

    test

---
+++ J07001.java
@@ -0,0 +1,21 @@
+package File;
+
+/**
+ *
+ * @author Hoàng Hướng
+ */
+
+import java.util.*;
+import java.io.*;
+
+public class J07001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        while(sc.hasNextLine()) System.out.println(sc.nextLine());
+    }
+    
+}
