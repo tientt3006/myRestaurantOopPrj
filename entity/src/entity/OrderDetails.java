057ed5efd347a4687ddab33731b031c673de4ab6
Heiuhoccode <danghieu121020@gmail.com>
2024-10-09 22:33:09 +0700

    haha

---
+++ entity/src/entity/OrderDetails.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+import java.io.*;
+import java.util.*;
+public class OrderDetails implements Serializable{
+    private int order_detail_id;
+    private int order_id;
+    private int dish_id;
+    private int quantity;
+    private float price;
+    
+}
+
