49db13c0ea348fbfc76931e444f8f7197e832ec1
tienbeo3006 <chunchunmaru.vn@gmail.com>
2024-10-11 01:17:04 +0700

    note cho Hieu trong file Experiment_User.java

--- src/main/java/com/nolaneg/myrestaurantprj/db/entity/Experiment_User.java
+++ src/main/java/com/nolaneg/myrestaurantprj/db/entity/Experiment_User.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
+// Dung xoa cac file co phan dau Experiment_ nhe, de thu nghiem da
 package com.nolaneg.myrestaurantprj.db.entity;
 import java.util.*;
 import java.io.*;
 import java.math.*;
 
-/**
- *
- * @author $_{user}
- */
 public class Experiment_User {
     private int id;
     private String username;
