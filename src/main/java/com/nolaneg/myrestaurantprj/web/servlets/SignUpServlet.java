dcf033c45735dd7dc203b950e08274462603b7e2
tienbeo3006 <chunchunmaru.vn@gmail.com>
2024-10-23 09:45:11 +0700

    modify sigupservlet.java with @WebServlet, lack of slash sign

--- src/main/java/com/nolaneg/myrestaurantprj/web/servlets/SignUpServlet.java
+++ src/main/java/com/nolaneg/myrestaurantprj/web/servlets/SignUpServlet.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@WebServlet("signup")
+@WebServlet("/signup")
 public class SignUpServlet extends HttpServlet {
 
     private static final Logger logger = LoggerFactory.getLogger(SqlUtils.class);
