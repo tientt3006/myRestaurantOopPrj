88fec58736964f06b242152662656db47760fd33
tienbeo3006 <chunchunmaru.vn@gmail.com>
2024-10-30 23:21:42 +0700

    add dish photo

--- src/main/java/com/nolaneg/myrestaurantprj/web/filters/MenuFilter.java
+++ src/main/java/com/nolaneg/myrestaurantprj/web/filters/MenuFilter.java
@@ -7,11 +7,15 @@ package com.nolaneg.myrestaurantprj.web.filters;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebFilter;
-import jakarta.servlet.http.HttpFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
+import com.nolaneg.myrestaurantprj.db.entity.User;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *
@@ -19,11 +23,13 @@ import jakarta.servlet.http.HttpServletResponse;
  */
 @WebFilter({"/","/menu"})
 public class MenuFilter extends HttpFilter {
-    protected void doFilter(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         int category = req.getIntHeader("category");
         String sort = req.getParameter("sort");
         if (category == 0 || sort == null) {
             res.sendRedirect(req.getContextPath() + "/menu?category=0&sort=categoryId&page=0&dishesOnPage=5");
+        }else {
+            chain.doFilter(req, res);
         }
     }
 }
