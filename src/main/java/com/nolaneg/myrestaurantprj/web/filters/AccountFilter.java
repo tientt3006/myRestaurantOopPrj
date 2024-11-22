aae355ab17985014e8bad1f7b49a97b85f9b6fdb
tienbeo3006 <chunchunmaru.vn@gmail.com>
2024-11-22 13:44:01 +0700

    Modified AccountFilter.java, still not fixed: connection full

--- src/main/java/com/nolaneg/myrestaurantprj/web/filters/AccountFilter.java
+++ src/main/java/com/nolaneg/myrestaurantprj/web/filters/AccountFilter.java
@@ -1,29 +1,29 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//
-//package com.nolaneg.myrestaurantprj.web.filters;
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-///**
-// *
-// * @author $_{user}
-// */
-//@WebFilter({"/account", "/account/change_password", "/account/change_info"})
-//public class AccountFilter extends HttpFilter {
-//
-//    @Override
-//    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-//        if (req.getSession().getAttribute("user") == null) {
-//            res.sendRedirect(req.getContextPath() + "/login");
-//        } else {
-//            chain.doFilter(req, res);
-//        }
-//    }
-//}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.nolaneg.myrestaurantprj.web.filters;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+/**
+ *
+ * @author $_{user}
+ */
+@WebFilter({"/account"})
+public class AccountFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        if (req.getSession().getAttribute("user") == null) {
+            res.sendRedirect(req.getContextPath() + "/login");
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+}
